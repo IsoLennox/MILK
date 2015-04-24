d0025e96bb675d5ba5f5106ceedf39ee2e7d3f54
Isobel Lennox <isolennox@gmail.com>
2015-04-23 17:02:09 -0700

    Update index.php
    Testing fetch

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include("inc/header.php"); ?>
 
  
-<!--if client: show client home page, else: show employee home page-->
+<!--if client: show client home page, else: show employee home page!-->
         
       <h1>Home Page</h1>
         
