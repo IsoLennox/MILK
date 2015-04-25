c6585865bda8876bb65c643894f0d90093a15f8f
Isobel Lennox <isolennox@gmail.com>
2015-04-25 11:26:26 -0700

    made placeholder note

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php include("inc/header.php"); ?>
 
-<!-- TESTING -->
+<!-- HOTFIX -->
 <!--if client: show client home page, else: show employee home page.-->
 
 <h1>Home Page</h1>
+       
+       <p>Statistics</p>
         
 <?php include("inc/footer.php"); ?>
