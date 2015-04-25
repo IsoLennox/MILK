2eaa7f90acef2e31e1f9c46ab9919400ba4ca19f
Isobel Lennox <isolennox@gmail.com>
2015-04-25 11:04:41 -0700

    switch branch testing

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
  
 <!--if client: show client home page, else: show employee home page!-->
         
-      <h1>Home Page</h1>
+      <h1>Homer Page</h1>
         
 <?php include("inc/footer.php"); ?>
+
+
+
+
