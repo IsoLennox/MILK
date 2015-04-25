8c838771f83528c4344a509951c031ec54f277d8
Isobel Lennox <isolennox@gmail.com>
2015-04-25 13:57:00 -0700

    techhub branch

--- index.php
+++ index.php
@@ -4,5 +4,9 @@
 <!--if client: show client home page, else: show employee home page!-->
         
       <h1>Home Page</h1>
+
+Infiltrating from the tech hub!
         
 <?php include("inc/footer.php"); ?>
+
+
