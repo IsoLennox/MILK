fa174b0f2d89f85df6ff1e0dff7eaed3585ed979
Isobel Lennox <isolennox@gmail.com>
2015-04-25 10:57:01 -0700

    testing branches

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php include("inc/header.php"); ?>
 
  
-<!--if client: show client home page, else: show employee home page!-->
-        
-      <h1>Home Page</h1>
+<!--if client: show client home page, else: show employee home page.-->
+
+<h1>Home Page</h1>
         
 <?php include("inc/footer.php"); ?>
