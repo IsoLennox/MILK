f56ed679abdb7963efa159d536771a4b6341a54d
Isobel Lennox <isolennox@gmail.com>
2015-05-03 15:02:03 -0700

    Update db_connection.php

--- inc/db_connection.php
+++ inc/db_connection.php
@@ -2,9 +2,9 @@
  
 
 define("DB_SERVER","localhost");
-define("DB_USER","ilennox");
-define("DB_PASS","z107*2013");
-define("DB_NAME","ilennox_milk");
+define("DB_USER","*********************");
+define("DB_PASS","**********************);
+define("DB_NAME","**********************");
 
 $connection= mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
