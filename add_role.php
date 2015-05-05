a3318f8fcf825ac0ba9abc56d98c1cefcdc97be4
Isobel Lennox <isolennox@gmail.com>
2015-05-05 15:02:20 -0700

    Added Role Creator

--- add_role.php
+++ add_role.php
@@ -2,19 +2,49 @@
 
 
 <h1>New Role</h1> 
-<!-- <form action="add_role.php">
-    Name: <input type="text" name="role_name">
-    Permissions:
-        <input type="checkbox" name="permission[]">Permission
-        <input type="checkbox" name="permission[]">Permission
-        <input type="checkbox" name="permission[]">Permission
-        <input type="checkbox" name="permission[]">Permission
-        <input type="checkbox" name="permission[]">Permission
-        <input type="submit" name="submit" id="submit" value="Save Role">
-</form> -->
 
-<form action="add_role.php">
-    Name: <input type="text" name="role_name">
+<?php
+if (isset($_POST['submit'])) {
+    $name= $_POST['name'];
+    $permissions_array= $_POST['permission'];
+    
+    
+        //INSERT ALL DATA EXCEPT PERMISSIONS
+    $insert  = "INSERT INTO roles ( name ) VALUES ( '{$name}' ) ";
+    $insert_result = mysqli_query($connection, $insert);
+    if($insert_result){
+    
+            //GET ID OF ROLE
+            $id_query  = "SELECT * FROM roles WHERE name= '{$name}' ORDER BY id DESC LIMIT 1 ";
+            $id_query_result = mysqli_query($connection, $id_query);
+            if($id_query_result){
+                        $result_array=mysqli_fetch_assoc($id_query_result);
+                        $role_id=$result_array['id'];
+
+                    //EXPLODE permissions ARRAY: FOREACH, INSERT INTO permissions_roles TABLE    
+                    for ($i = 0; $i < count($permissions_array); $i++) {  
+                            $insert_permission_query  = "INSERT INTO roles_permissions (permission_id, role_id) VALUES ('{$permissions_array[$i]}', '{$role_id}')";
+                            $insert_permission_query_result = mysqli_query($connection, $insert_permission_query);
+                                } 
+                
+                          $_SESSION["message"] = "New Role Created!";
+                          redirect_to("roles.php");
+
+                    }else{
+                
+                    $_SESSION["message"] = "New Role Created, Could Not Find new role! Permissions not added.";
+                    redirect_to("add_role.php");
+                }
+        }else{
+            $_SESSION["message"] = "New Role Failed!";
+            redirect_to("add_role.php");
+    }
+            
+    }
+?>
+
+<form action="add_role.php" method="POST">
+    Name: <input type="text" name="name" id="name" value="" > 
     Permissions:
                <?php
 
@@ -28,8 +58,8 @@
        foreach($result as $show){
            
            $role_id = $show['id'];
-           $role_name = $show['name'];
-           echo "<input type=\"checkbox\" name=\"permission[]\">" . $role_name."<br/>";
+           $role_name = $show['name']; 
+           echo "<input type=\"checkbox\" name=\"permission[]\" value=\"".$role_id."\"  >".$role_name."<br/>"; 
                      
            }
        }
