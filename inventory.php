873b09c92a309582ae5d48180a7c35556740355c
Isobel Lennox <isolennox@gmail.com>
2015-05-08 17:13:19 -0700

    completed room CRUD

--- inventory.php
+++ inventory.php
@@ -2,24 +2,28 @@
 
 
 <h1>Inventory</h1>
- <p>list of your stuff:</p>
-<a href="item_details.php?id=1">Sample Item</a>
-           
-           <?php
 
-//example query
+<a href="add_item.php">+ Add Item</a>
+ <h2>Your Items:</h2>
 
-//    $query  = "SELECT * FROM TABLE WHERE user_id={$_SESSION['user_id']}";  
-//    $result = mysqli_query($connection, $query);
-//    if($result){
-//        //show each result value
-//        foreach($result as $show){
-//            
-//            $this_value=$show['col_name'];
-//            echo $this_value;
-//                      
-//            }
-//        }
+  <?php
+
+//Get items
+
+    $query  = "SELECT * FROM items WHERE user_id={$_SESSION['user_id']}";  
+    $result = mysqli_query($connection, $query);
+    if($result){
+        //show each result value
+        echo "<ul>";
+        foreach($result as $show){
+            
+            $id=$show['id'];
+            $name=$show['name'];
+            echo "<li><a href=\"item_details.php?id=".$id."\">".$name."</a></li>";
+                      
+            }
+        echo "</ul>";
+        }
  ?>
      
         
