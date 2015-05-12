ad6529ac74c704f38ef24dd37c82c5316118ef0d
kriskuchinka <kriskuchinka@gmail.com>
2015-05-12 11:58:05 -0700

    morem ipsum

--- help.php
+++ help.php
@@ -9,6 +9,7 @@
  </ul>
     <h1>Take the walkthrough again</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla laboriosam animi neque, dignissimos aut possimus amet facilis optio dolorem, ipsam cumque tempore minima sunt ipsa. Nihil consequuntur, in possimus praesentium.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla laboriosam animi neque, dignissimos aut possimus amet facilis optio dolorem, ipsam cumque tempore minima sunt ipsa. Nihil consequuntur, in possimus praesentium.</p>
      
         
       
