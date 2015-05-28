3412bcf485e899470f76b330d4e0c52095592183
LaRhea P <larhea.phillips@yahoo.com>
2015-05-28 03:34:14 -0700

    chart styling

--- client_dashboard.php
+++ client_dashboard.php
@@ -149,7 +149,7 @@ if($num_rooms==0){
 
             echo "</div>"; //end half
 if(!empty($total_items)){
-            echo "<span class=\"half_div\">"; 
+            echo "<div class=\"half_div\">"; 
                 //SHOW RECENTLY ADDED ITEMS
                 $recent_item_query  =  "SELECT * FROM items WHERE user_id={$_SESSION['user_id']} AND in_trash=0 ORDER BY id DESC LIMIT 10";   
                 $recent_item_result = mysqli_query($connection, $recent_item_query);
@@ -158,12 +158,12 @@ if(!empty($total_items)){
                     echo "<ul>";
                 foreach($recent_item_result as $item){ echo "<li><a href=\"item_details.php?id=".$item['id']."\">".$item['name']."</a></li>"; }
                     echo "</ul>";
-                    echo "<br/><p><a class=\"right\" href=\"inventory.php\">View all items <i class=\"fa fa-angle-double-right\"></i></a></p>";
+                    
                     }//end get recent items
 
-            echo "</span>"; //end half
-}
-    
+            echo "</div>"; //end half
+}           echo "<div class=\"clearfix\"></div>";
+            echo "<br/><p><a class=\"right\" href=\"inventory.php\">View all items <i class=\"fa fa-angle-double-right\"></i></a></p>";
        
        echo "</div>";
 
@@ -220,41 +220,41 @@ if(!empty($total_items)){
             $ddata=mysqli_fetch_assoc($denied_result); 
         ?>
           
-          <span class="half_div">
+        <div class="claims_dashboard half_div">
               
-            <a href="claim_history.php"><i class="fa fa-folder-open"></i> All Claims</a> (<?php echo $data['total']; ?>)<br/>
-            <a href="claim_history.php?approved"><i class="fa fa-check green"></i> Approved </a> (<?php echo $adata['total']; ?>)<br/>
-            <a href="claim_history.php?denied"><i class="fa fa-times red"></i> Denied </a> (<?php echo $ddata['total']; ?>)<br/>             
-            <a href="claim_history.php?pending"><i class="fa fa-clock-o "></i> Processing </a> (<?php echo $pdata['total']; ?>)<br/>            
-            <a href="claim_history.php?changes"><i class="fa fa-pencil"></i> Pending Changes </a> (<?php echo $cdata['total']; ?>) <br/>
-            <a href="claim_history.php?draft"><i class="fa fa-file-o "></i> Drafts </a> (<?php echo $drdata['total']; ?>) <br/>
+            <p><a href="claim_history.php"><i class="fa fa-folder-open"></i> All Claims</a> (<?php echo $data['total']; ?>)</p>
+            <p><a href="claim_history.php?approved"><i class="fa fa-check green"></i> Approved </a> (<?php echo $adata['total']; ?>)</p>
+            <p><a href="claim_history.php?denied"><i class="fa fa-times red"></i> Denied </a> (<?php echo $ddata['total']; ?>)</p>             
+            <p><a href="claim_history.php?pending"><i class="fa fa-clock-o "></i> Processing </a> (<?php echo $pdata['total']; ?>)</p>            
+            <p><a href="claim_history.php?changes"><i class="fa fa-pencil"></i> Pending Changes </a> (<?php echo $cdata['total']; ?>) </p>
+            <p><a href="claim_history.php?draft"><i class="fa fa-file-o "></i> Drafts </a> (<?php echo $drdata['total']; ?>) </p>
             
-            </span>
+        </div>
             
             
-            <span class="half_div">
+            <div class="half_div">
 <!--            IF ANY DRAFTS, SHOW BY ID ASC-->
    <?php
     $get_draft  = "SELECT * FROM claims WHERE user_id={$_SESSION['user_id']} AND status_id=1 ORDER BY id LIMIT 1";  
     $draft_result = mysqli_query($connection, $get_draft); 
     $draft_rows=mysqli_num_rows($draft_result);
     if($draft_rows >=1){
-        echo "<h2>Finish This Draft</h2><br/>";
+        echo "<h2>Finish This Draft</h2>";
          $draft_details=mysqli_fetch_assoc($draft_result);
-        echo "<h4>".$draft_details['title']."</h4>";
-        echo "<p>".$draft_details['notes']."</p>";
-        echo "<a href=\"claim_details.php?id=".$draft_details['id']."\">Edit Claim</a>";
+        echo "<h3>".$draft_details['title']."</h3>";
+        echo "<p>".$draft_details['notes']."</p><br>";
+        echo "<p><a href=\"claim_details.php?id=".$draft_details['id']."\"><i class=\"fa fa-pencil\"></i> Edit Claim</a></p>";
     }else{
 //    IF NO DRAFTS, SHOW LAST CLAIM SUBMITTED, AND ITS STATUS
-        echo "<h2>Most Recent Claim</h2><br/>";
+        echo "<h2>Most Recent Claim</h2>";
         $get_claim  = "SELECT * FROM claims WHERE user_id={$_SESSION['user_id']} ORDER BY id LIMIT 1";  
         $claim_result = mysqli_query($connection, $get_claim); 
         $claim_rows=mysqli_num_rows($claim_result);
         if($claim_rows >=1){
              $claim_details=mysqli_fetch_assoc($claim_result);
-            echo "<h4>".$claim_details['title']."</h4>";
-            echo "<p>".$claim_details['notes']."</p>";
-            echo "<a href=\"claim_details.php?id=".$claim_details['id']."\">View Claim</a>";
+            echo "<h3>".$claim_details['title']."</h3>";
+            echo "<p>".$claim_details['notes']."</p><br>";
+            echo "<p><a href=\"claim_details.php?id=".$claim_details['id']."\">View Claim</a></p>";
 
         }else{
 
@@ -265,7 +265,7 @@ if(!empty($total_items)){
     }
 ?> 
             
-</span>
+</div>
             
             
 <!--            MOST RECENTLY ADDED ITEMS -->
