089ba688584f8226a22f0354babd2a3d39813a63
Isobel Lennox <isolennox@gmail.com>
2015-05-27 17:13:47 -0700

    styled edit item form to look like add item form

--- edit_item.php
+++ edit_item.php
@@ -59,60 +59,59 @@ if(isset($_POST['submit'])){
 
 
 <h1>Editing <?php echo $name['name']; ?></h1> 
-
-
-
-<form method="POST" >
-               
+ 
+    <form class='add_item' method="POST" >
+              
+    <label for="name"> Item Name: </label><input type="text"  style="border: 2px solid #90C32E;" id='name' name="name" placeholder=" i.e. Samsung Television" value="<?php echo $name['name']; ?>" > 
      <!--    //GET ITEM CATEGORIES -->
    <?php
-                $category_query  = "SELECT * FROM item_category"; 
-                $categoryresult = mysqli_query($connection, $category_query);
-                if($categoryresult){ 
-                    //CATEGORY SELECT BOX
-                    echo "<p>Category:  <select name=\"category\" >";
-                    foreach($categoryresult as $category){
-                        //OPTIONS
-                        
-                        if($category['id']==$name['category']){ $selected = "selected";}else{ $selected="";}
-                        echo "<option ".$selected." value=\"".$category['id']."\" >".$category['name']."</option>";  
-                    }
-                    echo "</select></p>";
-                }//end get categories
+    $category_query  = "SELECT * FROM item_category"; 
+    $categoryresult = mysqli_query($connection, $category_query);
+    if($categoryresult){ 
+        //CATEGORY SELECT BOX 
+        echo "<label for=\"category\">Item Category: </label><select style=\"border: 2px solid #90C32E;\" id='category' name=\"category\">";
+         echo "<option value=\"--\" >--Select Item Category--</option>";
+        foreach($categoryresult as $category){
+            //OPTIONS
+            if($category['id']==$name['category']){ $selected = "selected";}else{ $selected="";}
+                        echo "<option ".$selected." value=\"".$category['id']."\" >".$category['name']."</option>"; 
+        }
+        echo "</select>";
+    }//end get categories
 
 ?> 
-    <p>Item Title: <input type="text" name="name" placeholder="i.e. Samsung Television" value="<?php echo $name['name']; ?>" ></p>
-    
 <!--    //GET ROOMS TO CHOOSE FROM -->
    <?php
-                $room_query  = "SELECT * FROM rooms WHERE user_id={$_SESSION['user_id']}"; 
-                $roomresult = mysqli_query($connection, $room_query);
-                if($roomresult){ 
-                    //ROOM SELECT BOX
-                    echo "<p>Room: <select name=\"room\" >";
-                    foreach($roomresult as $room){
-                        //OPTIONS
-                         if($room['id']==$name['room_id']){ $selected = "selected";}else{ $selected="";}
+        $room_query  = "SELECT * FROM rooms WHERE user_id={$_SESSION['user_id']}"; 
+        $roomresult = mysqli_query($connection, $room_query);
+        if($roomresult){ 
+            //ROOM SELECT BOX
+            echo "<label for=\"room\">Room: </label><select id='room' name=\"room\" >";
+            foreach($roomresult as $room){
+                //OPTIONS
+                if($room['id']==$name['room_id']){ $selected = "selected";}else{ $selected="";}
                         echo "<option ".$selected." value=\"".$room['id']."\" >".$room['name']."</option>"; 
-                        
-                    }
-                    echo "</select></p>";
-                }//end get rooms 
+            }
+            echo "</select>";
+        }//end get rooms 
  ?>
-   
-   <p>Item Description/Notes: <textarea name="notes" id="notes" cols="30" rows="10" value="<?php echo $name['notes']; ?>"><?php echo $name['notes']; ?></textarea></p>
-     
-    <p>Purchase Date: <input type="text" name="purchase_date" placeholder="mm/dd/yyyy" value="<?php echo $name['purchase_date']; ?>"></p>
-    <p>Purchase Price: $<input type="text" name="purchase_price" placeholder="950.89" value="<?php echo $name['purchase_price']; ?>"></p>
-    <p>Declared Value: $<input type="text" name="declared_value" placeholder="950.99" value="<?php echo $name['declared_value']; ?>"></p>
-   
-    <p>Add File (e.x. Image/file of object, reciept, appraisal... )</p>
-    <input type="hidden" name="item_id" value="<?php echo $name['id']; ?>">
-    <input type="submit" name="submit" value="Save Item">
- </form>
-     
+    <fieldset class='form_blocks'>
+        <label for="notes">Item Description/Notes: </label><textarea name="notes" id="notes" cols="30" rows="12" value="<?php echo $name['notes']; ?>"><?php echo $name['notes']; ?></textarea>
+    </fieldset>
+
+    <fieldset class='form_blocks'>
+        <label for="purchase_date">Purchase Date: </label><input type="text" id="purchase_date" name="purchase_date" placeholder="mm/dd/yyyy" value="<?php echo $name['purchase_date']; ?>">
+        <label for="purchase_price">Purchase Price: $</label><input type="text" id="purchase_price" name="purchase_price" placeholder="950" value="<?php echo $name['purchase_price']; ?>">
+        <label for="declared_value">Declared Value: $</label><input type="text" id="declared_value" name="declared_value" placeholder="950" value="<?php echo $name['declared_value']; ?>">
+        
          
-<a href="item_details.php?id=<?php echo $id; ?>"  onclick="return confirm('Leave the page? This will not save your item!')" >Cancel</a>
+    </fieldset>
+    <input type="hidden" name="item_id" value="<?php echo $name['id']; ?>">
+    <input type="submit" name="submit" value="Next"><a href="item_details.php?id=<?php echo $id; ?>" onclick="return confirm('Leave the page? This will not save your item!');"><i class="fa fa-times"></i> Cancel</a> 
+ </form>
+    
+    
+     
            <?php } ?>
            
            
