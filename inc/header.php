22f72e8052b3644d6329404017d4385d036e22b1
Isobel Lennox <isolennox@gmail.com>
2015-05-27 18:36:47 -0700

    created 'undo' feature when marking alerts as read

--- inc/header.php
+++ inc/header.php
@@ -318,5 +318,6 @@ if($_SESSION['is_employee']==0){
 
         <div class="clearfix" id="page"> 
               <?php echo message(); ?>
+              <?php echo errors(); ?>
               <?php echo form_errors($errors); ?>
               <div class="content_wrapper">
